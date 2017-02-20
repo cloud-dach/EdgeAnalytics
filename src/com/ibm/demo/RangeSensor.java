cc81d36b49af14e82841d4c9197e9fb4824eff64
rhocheck <rainer.hochecker@de.ibm.com>
2017-02-20 07:44:31 +0100

    fix indentation

--- src/com/ibm/demo/RangeSensor.java
+++ src/com/ibm/demo/RangeSensor.java
@@ -13,112 +13,112 @@ import com.pi4j.io.gpio.*;
 import com.pi4j.wiringpi.GpioUtil;
 
 public class RangeSensor extends StringConnector implements Runnable {
-	
-    private static final Double SPEED_OF_SOUND = 343.0;
-    private GpioController gpio = GpioFactory.getInstance();
-    private Pin echoRPin = RaspiPin.GPIO_27;
-    private Pin trigRPin = RaspiPin.GPIO_26;
-    GpioPinDigitalInput echoPin = null;
-    GpioPinDigitalOutput trigPin = null; 
-    private Consumer<Double> eventSubmitter;
-    private Thread t;
-    
-    public RangeSensor() {
-        GpioUtil.enableNonPrivilegedAccess();
-        echoPin = gpio.provisionDigitalInputPin(echoRPin);
-        trigPin = gpio.provisionDigitalOutputPin(trigRPin);
-    }
-    
+
+	private static final Double SPEED_OF_SOUND = 343.0;
+	private GpioController gpio = GpioFactory.getInstance();
+	private Pin echoRPin = RaspiPin.GPIO_27;
+	private Pin trigRPin = RaspiPin.GPIO_26;
+	GpioPinDigitalInput echoPin = null;
+	GpioPinDigitalOutput trigPin = null; 
+	private Consumer<Double> eventSubmitter;
+	private Thread t;
+
+	public RangeSensor() {
+		GpioUtil.enableNonPrivilegedAccess();
+		echoPin = gpio.provisionDigitalInputPin(echoRPin);
+		trigPin = gpio.provisionDigitalOutputPin(trigRPin);
+	}
+
 	@Override
 	public org.apache.edgent.topology.TStream<java.lang.String> getStream(org.apache.edgent.topology.Topology t) {
-        //poll Range sensor every second for distance reading
-        TStream<Double> distanceReadings = t.events(eventSubmitter -> register(eventSubmitter));
-       
-        //filter out bad readings that are out of the sensor's 1000cm range
-        distanceReadings = distanceReadings.filter(j -> j < 1.00);
+		//poll Range sensor every second for distance reading
+		TStream<Double> distanceReadings = t.events(eventSubmitter -> register(eventSubmitter));
 
-        TStream<JsonObject> sensorJSON = distanceReadings.map(v -> {
-            JsonObject j = new JsonObject();
-            j.addProperty("name", "rangeSensor");
-            j.addProperty("reading", v);
-            return j;
-        });
+		//filter out bad readings that are out of the sensor's 1000cm range
+		distanceReadings = distanceReadings.filter(j -> j < 1.00);
 
-        // Create a window on the stream of the last 10 seconds
-        TWindow<JsonObject,JsonElement> sensorWindow = sensorJSON.last(10, TimeUnit.SECONDS, j -> j.get("name"));
+		TStream<JsonObject> sensorJSON = distanceReadings.map(v -> {
+			JsonObject j = new JsonObject();
+			j.addProperty("name", "rangeSensor");
+			j.addProperty("reading", v);
+			return j;
+		});
 
-        // Aggregate the windows calculating the min, max, mean and standard deviation
-        // across each window independently.
-        sensorJSON = JsonAnalytics.aggregate(sensorWindow, "name", "reading", Statistic.MIN, Statistic.MAX, Statistic.MEAN, Statistic.STDDEV);
+		// Create a window on the stream of the last 10 seconds
+		TWindow<JsonObject,JsonElement> sensorWindow = sensorJSON.last(10, TimeUnit.SECONDS, j -> j.get("name"));
 
-        TStream<JsonObject> results = sensorJSON.map(v -> {
-        	return v.get("reading").getAsJsonObject();
-        });
+		// Aggregate the windows calculating the min, max, mean and standard deviation
+		// across each window independently.
+		sensorJSON = JsonAnalytics.aggregate(sensorWindow, "name", "reading", Statistic.MIN, Statistic.MAX, Statistic.MEAN, Statistic.STDDEV);
+
+		TStream<JsonObject> results = sensorJSON.map(v -> {
+			return v.get("reading").getAsJsonObject();
+		});
 		return results.asString();
 	}
 
-    public void register(Consumer<Double> eventSubmitter) {
-        this.eventSubmitter = eventSubmitter;
-        
-        t = new Thread (this, "reader");
-        t.setPriority(Thread.NORM_PRIORITY + 1);
-        t.start();
-    }
+	public void register(Consumer<Double> eventSubmitter) {
+		this.eventSubmitter = eventSubmitter;
+
+		t = new Thread (this, "reader");
+		t.setPriority(Thread.NORM_PRIORITY + 1);
+		t.start();
+	}
 
 	@Override
 	public void run() {
 		// TODO Auto-generated method stub
-        System.out.println("Start reader thread!");
-        
-        while (true) {
-            eventSubmitter.accept(getDistance());
-            try {
-                Thread.sleep(2000);
-            } catch (InterruptedException e1) {
-                // TODO Auto-generated catch block
-                e1.printStackTrace();
-            }
+		System.out.println("Start reader thread!");
 
-        }
+		while (true) {
+			eventSubmitter.accept(getDistance());
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e1) {
+				// TODO Auto-generated catch block
+				e1.printStackTrace();
+			}
+
+		}
 	}
-	
-    private Double getDistance() {
 
-        trigPin.low();
-        try {
-            TimeUnit.MICROSECONDS.sleep(5);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        trigPin.high();
-        try {
-            TimeUnit.MICROSECONDS.sleep(10);
-        } catch (InterruptedException e) {
-            // TODO Auto-generated catch block
-            e.printStackTrace();
-        }
-        trigPin.low();
-        
-        int MAX_CYCLES = 10000;
-        int cycles = 0;
-        while (this.echoPin.isLow() && cycles < MAX_CYCLES) {
-            cycles++;
-        }
-        
-        long start = System.nanoTime();
-        cycles = 0;
-        while (this.echoPin.isHigh() && cycles < MAX_CYCLES) {
-            cycles++;
-        }
-        long end = System.nanoTime();
+	private Double getDistance() {
 
-        if (cycles >= 10000) {
-            return 10.0;
-        }
+		trigPin.low();
+		try {
+			TimeUnit.MICROSECONDS.sleep(5);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		trigPin.high();
+		try {
+			TimeUnit.MICROSECONDS.sleep(10);
+		} catch (InterruptedException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		trigPin.low();
 
-        Double reboundTimeMicroSeconds = (Double)Math.ceil((end - start) / 1000.0);
-        Double distance = reboundTimeMicroSeconds / 1000000 * SPEED_OF_SOUND / 2;
-        return distance;
-    }
+		int MAX_CYCLES = 10000;
+		int cycles = 0;
+		while (this.echoPin.isLow() && cycles < MAX_CYCLES) {
+			cycles++;
+		}
+
+		long start = System.nanoTime();
+		cycles = 0;
+		while (this.echoPin.isHigh() && cycles < MAX_CYCLES) {
+			cycles++;
+		}
+		long end = System.nanoTime();
+
+		if (cycles >= 10000) {
+			return 10.0;
+		}
+
+		Double reboundTimeMicroSeconds = (Double)Math.ceil((end - start) / 1000.0);
+		Double distance = reboundTimeMicroSeconds / 1000000 * SPEED_OF_SOUND / 2;
+		return distance;
+	}
 }
--- src/com/ibm/demo/RaspiEAA.java
+++ src/com/ibm/demo/RaspiEAA.java
@@ -24,96 +24,96 @@ import com.ibm.iotf.devicemgmt.DeviceFirmware.FirmwareState;
 import com.ibm.iotf.devicemgmt.gateway.ManagedGateway;
 
 public class RaspiEAA {
-	  private static final Logger LOGGER = LoggerFactory.getLogger(RaspiEAA.class);
+	private static final Logger LOGGER = LoggerFactory.getLogger(RaspiEAA.class);
 
-	  public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 		String deviceCfg = args[0];
-	    Properties props = loadProperties(deviceCfg);
-	    String ORG_ID = props.getProperty("Organization-ID");
-	    String AUTH_KEY = props.getProperty("API-Key");
-	    String AUTH_TOKEN = props.getProperty("API-Token");
+		Properties props = loadProperties(deviceCfg);
+		String ORG_ID = props.getProperty("Organization-ID");
+		String AUTH_KEY = props.getProperty("API-Key");
+		String AUTH_TOKEN = props.getProperty("API-Token");
 
-	    String APP_ID = props.getProperty("Application-ID");
-	    String GATEWAY_ID = props.getProperty("Gateway-ID");
-	    String GATEWAY_TYPE = props.getProperty("Gateway-Type");
-	    String GATEWAY_AUTH_TOKEN = props.getProperty("Authentication-Token");
+		String APP_ID = props.getProperty("Application-ID");
+		String GATEWAY_ID = props.getProperty("Gateway-ID");
+		String GATEWAY_TYPE = props.getProperty("Gateway-Type");
+		String GATEWAY_AUTH_TOKEN = props.getProperty("Authentication-Token");
 
-	    EdgeProperties edgeProps = new EdgeProperties(ORG_ID, APP_ID, AUTH_KEY, AUTH_TOKEN,
-	        GATEWAY_TYPE, GATEWAY_ID, GATEWAY_AUTH_TOKEN);
+		EdgeProperties edgeProps = new EdgeProperties(ORG_ID, APP_ID, AUTH_KEY, AUTH_TOKEN,
+				GATEWAY_TYPE, GATEWAY_ID, GATEWAY_AUTH_TOKEN);
 
-	    EdgeAnalyticsClient edgeAnalyticsClient = new EdgeAnalyticsClient(edgeProps);
-	    EdgeAnalyticsAgent eaa = edgeAnalyticsClient.getAgent();
+		EdgeAnalyticsClient edgeAnalyticsClient = new EdgeAnalyticsClient(edgeProps);
+		EdgeAnalyticsAgent eaa = edgeAnalyticsClient.getAgent();
 
-	    // Start edge analytics agent with the new gateway.
-	    eaa.start();
+		// Start edge analytics agent with the new gateway.
+		eaa.start();
 
-	    // (Optional) Register an alert handler
-	    eaa.registerDeviceActionHandler(new AlertFileActionHandler("ABC.CSV"));
+		// (Optional) Register an alert handler
+		eaa.registerDeviceActionHandler(new AlertFileActionHandler("ABC.CSV"));
 
-	    // Constructor connector and send events
-	    final StringConnector connector = new RangeSensor();
-	    connector.setTransformer(new JsonTransformer());
-	    eaa.deviceData("RangeDeviceId", "RangeDeviceType", "eventType", connector);
-	    ManagedGateway managedGW = getManagedGateway(eaa.getMqttAsyncClient(), GATEWAY_TYPE, GATEWAY_ID);
-	    managedGW.sendDeviceManageRequest("RangeDeviceType", "RangeDeviceId", 0, true, true);
-	    
-	    // Publish a event every 5 seconds.
-	    int i = 0;
-	    while (i++ < 100000) {
-	      //Event event = new Event("SampleStringDeviceId", "SampleStringDeviceType", "{\"Hello\": \"World!\"}");
-	      //eaa.deviceData(event);
-	      //LOGGER.info("Sent message {} successfully to EAA", event);
+		// Constructor connector and send events
+		final StringConnector connector = new RangeSensor();
+		connector.setTransformer(new JsonTransformer());
+		eaa.deviceData("RangeDeviceId", "RangeDeviceType", "eventType", connector);
+		ManagedGateway managedGW = getManagedGateway(eaa.getMqttAsyncClient(), GATEWAY_TYPE, GATEWAY_ID);
+		managedGW.sendDeviceManageRequest("RangeDeviceType", "RangeDeviceId", 0, true, true);
 
-	      if (i == 10) {
-	        LOGGER.info("Creating managed gatewayclient");
+		// Publish a event every 5 seconds.
+		int i = 0;
+		while (i++ < 100000) {
+			//Event event = new Event("SampleStringDeviceId", "SampleStringDeviceType", "{\"Hello\": \"World!\"}");
+			//eaa.deviceData(event);
+			//LOGGER.info("Sent message {} successfully to EAA", event);
 
-	      }
-	      Thread.sleep(5000);
-	    }
-	  }
+			if (i == 10) {
+				LOGGER.info("Creating managed gatewayclient");
 
-	  private static Properties loadProperties(String configPath) throws IOException {
-	    Properties props = new Properties();
-	    props.load(new FileInputStream(configPath));
-	    return props;
-	  }
+			}
+			Thread.sleep(5000);
+		}
+	}
 
-	  private static ManagedGateway getManagedGateway(MqttAsyncClient mqttClient, String gatewayType, String gatewayId) throws Exception {
-	    /**
-	     * To create a DeviceData object, we will need the following objects:
-	     *   - DeviceInfo
-	     *   - DeviceMetadata
-	     *   - DeviceLocation (optional)
-	     *   - DiagnosticErrorCode (optional)
-	     *   - DiagnosticLog (optional)
-	     *   - DeviceFirmware (optional)
-	     */
-	    DeviceInfo deviceInfo = new DeviceInfo.Builder().
-	        serialNumber("389999").
-	        build();
+	private static Properties loadProperties(String configPath) throws IOException {
+		Properties props = new Properties();
+		props.load(new FileInputStream(configPath));
+		return props;
+	}
 
-	    DeviceFirmware firmware = new DeviceFirmware.Builder().
-	        version("1.0").
-	        name("testname").
-	        state(FirmwareState.IDLE).
-	        build();
+	private static ManagedGateway getManagedGateway(MqttAsyncClient mqttClient, String gatewayType, String gatewayId) throws Exception {
+		/**
+		 * To create a DeviceData object, we will need the following objects:
+		 *   - DeviceInfo
+		 *   - DeviceMetadata
+		 *   - DeviceLocation (optional)
+		 *   - DiagnosticErrorCode (optional)
+		 *   - DiagnosticLog (optional)
+		 *   - DeviceFirmware (optional)
+		 */
+		DeviceInfo deviceInfo = new DeviceInfo.Builder().
+				serialNumber("389999").
+				build();
 
-	    /**
-	     * Create a DeviceMetadata object
-	     */
-	    JsonObject data = new JsonObject();
-	    data.addProperty("customField", "customValue");
-	    DeviceMetadata metadata = new DeviceMetadata(data);
+		DeviceFirmware firmware = new DeviceFirmware.Builder().
+				version("1.0").
+				name("testname").
+				state(FirmwareState.IDLE).
+				build();
 
-	    DeviceData deviceData = new DeviceData.Builder().
-	             typeId(gatewayType).
-	             deviceId(gatewayId).
-	             deviceInfo(deviceInfo).
-	             deviceFirmware(firmware).
-	             metadata(metadata).
-	             build();
+		/**
+		 * Create a DeviceMetadata object
+		 */
+		JsonObject data = new JsonObject();
+		data.addProperty("customField", "customValue");
+		DeviceMetadata metadata = new DeviceMetadata(data);
 
-	    System.out.println("test");
-	    return new ManagedGateway(mqttClient, deviceData);
-	  }
+		DeviceData deviceData = new DeviceData.Builder().
+				typeId(gatewayType).
+				deviceId(gatewayId).
+				deviceInfo(deviceInfo).
+				deviceFirmware(firmware).
+				metadata(metadata).
+				build();
+
+		System.out.println("test");
+		return new ManagedGateway(mqttClient, deviceData);
+	}
 }
